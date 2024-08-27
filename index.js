64af3806982c9ac587768dc9db6d303bde89ad4c
Nausu-98524 <112564419+Nausu-98524@users.noreply.github.com>
2024-08-28 00:09:46 +0530

    change index

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+app.get('/',(req, res)=>{
+    res.json("Hello")
+})
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
